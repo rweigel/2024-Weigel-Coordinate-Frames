e7ff2b950c95afa02a4201e7fbcedd9fefc169ac
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-06-12 16:31:08 -0400

    mms2

--- cpfigs.sh
+++ cpfigs.sh
@@ -1,1 +1,10 @@
-cp ../figures/pdf/Geotail_* figures
+D1=../figures/pdf
+D2=figures
+cp $D1/Geotail_SSCWeb-GEI_vs_CDAWeb-GCI.pdf $D2
+cp $D1/Geotail_SSCWeb-GSE_vs_CDAWeb-GSE.pdf $D2
+cp $D1/Geotail_SSCWeb-GSM_vs_CDAWeb-GSM.pdf $D2
+cp $D1/Geotail_SSCWeb-J2K_vs_CDAWeb-GCI.pdf $D2
+cp $D1/MMS-2_CDAWeb-GSE_vs_JPL-GSE.pdf $D2
+cp $D1/MMS-2_CDAWeb-GSM_vs_JPL-GSM.pdf $D2
+cp $D1/MMS-2_SSCWeb-GSE_vs_JPL-GSE.pdf $D2
+cp $D1/MMS-2_SSCWeb-GSM_vs_JPL-GSM.pdf $D2
